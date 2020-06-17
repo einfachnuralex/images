c9726ab16a48a2dc5d15cea2dc2010589c13992f
einfachnuralex <einfachnuralex@posteo.de>
2020-06-17 12:09:04 +0200

    Add env vars

--- downloader.sh
+++ downloader.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 # https://stable.release.flatcar-linux.net/amd64-usr/2512.2.0/flatcar_production_openstack_image.img.bz2
-echo test
-URL=https://stable.release.flatcar-linux.net
-ARCH=amd64-usr
-#VER=2512.2.0
-IMAGE=flatcar_production_openstack_image.img
-IMAGE_Z=flatcar_production_openstack_image.img.bz2
 DIGESTS_URL=$URL/$ARCH/$VER/$IMAGE_Z.DIGESTS
 IMAGE_URL=$URL/$ARCH/$VER/$IMAGE_Z
-
 echo $IMAGE_URL
 echo $DIGESTS_URL
 wget -q $IMAGE_URL
